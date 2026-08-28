14faa7712507edd9324cbe7b686720dcae8649ad
vpakspace <vpakspace@users.noreply.github.com>
2026-08-28 10:11:12 +0700

    feat: метод sayHunterReply в Welcomer и тест на слово hunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -15,4 +15,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunterReply(){
+		return "Fear the old blood, good hunter, and beware the beasts of the night.";
+	}
 }
